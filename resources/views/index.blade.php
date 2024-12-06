de158ed759122e2a433f0789bc6bbd63eaedbee0
IamJuanpax <jubritos@frba.utn.edu.ar>
2024-12-05 23:39:00 -0300

    detalles extra

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,91 +39,90 @@
         //    ARTUCLOS CON MENU DESPLEGABLE     //
         /////////////////////////////////////////-->
     
-<div class="container d-flex flex-column align-items-center">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <!-- Botón de colapso para mobile -->
-            <button class="navbar-toggler w-100" type="button" data-bs-toggle="collapse" data-bs-target="#menuArticulos" aria-controls="menuArticulos" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <div class="container d-flex flex-column align-items-center">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <!-- Botón de colapso para mobile -->
+                <button class="navbar-toggler w-100" type="button" data-bs-toggle="collapse" data-bs-target="#menuArticulos" aria-controls="menuArticulos" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <!-- Contenido del menú -->
-            <div class="collapse navbar-collapse" id="menuArticulos">
-                <ul class="container navbar-nav d-flex flex-column align-items-center">
-                    <li class="nav-item"><a class="nav-link" href="">OFERTAS</a></li>
-                    <li class="nav-item"><a class="nav-link" href="">REGALOS EMPRESARIALES</a></li>
-                    <li class="nav-item"><a class="nav-link" href="">CAMPING</a></li>
-                    <li class="nav-item"><a class="nav-link" href="">JARDIN</a></li>
-                    <li class="nav-item"><a class="nav-link" href="">ACCESORIOS PARA BICICLETAS</a></li>
-                    <li class="nav-item"><a class="nav-link" href="">ELECTROHOGAR</a></li>
-                    <li class="nav-item"><a class="nav-link" href="">ACCESORIOS PARA VEHICULOS</a></li>
-                    <li class="nav-item"><a class="nav-link" href="">HERRAMIENTAS</a></li>
-                    <li class="nav-item"><a class="nav-link" href="">MATAFUEGOS</a></li>
-                </ul>
+                <!-- Contenido del menú -->
+                <div class="collapse navbar-collapse" id="menuArticulos">
+                    <ul class="container navbar-nav d-flex flex-column align-items-center">
+                        <li class="nav-item"><a class="nav-link" href="">OFERTAS</a></li>
+                        <li class="nav-item"><a class="nav-link" href="">REGALOS EMPRESARIALES</a></li>
+                        <li class="nav-item"><a class="nav-link" href="">CAMPING</a></li>
+                        <li class="nav-item"><a class="nav-link" href="">JARDIN</a></li>
+                        <li class="nav-item"><a class="nav-link" href="">ACCESORIOS PARA BICICLETAS</a></li>
+                        <li class="nav-item"><a class="nav-link" href="">ELECTROHOGAR</a></li>
+                        <li class="nav-item"><a class="nav-link" href="">ACCESORIOS PARA VEHICULOS</a></li>
+                        <li class="nav-item"><a class="nav-link" href="">HERRAMIENTAS</a></li>
+                        <li class="nav-item"><a class="nav-link" href="">MATAFUEGOS</a></li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-</div>
+        </nav>
+    </div>
 
 
     <!-- /////////////////////////////////////////
         //               OFERTAS               //
         /////////////////////////////////////////-->
-<div class="container ofertas">
-    <a href="#" target="_blank">
-        <img src="images/promo.JPG" class="img-fluid" alt="oferta">
-    </a>
-</div>
+    <div class="container ofertas p-0">
+        <a href="#" target="_blank">
+            <img src="images/promo.JPG" class="img-fluid" alt="oferta">
+        </a>
+    </div>
 
     <!-- /////////////////////////////////////////
         //               CARRUSEL               //
         /////////////////////////////////////////-->
-<div id="carouselExampleIndicators" class="container carousel slide">
-    <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+    <div id="carouselExampleIndicators" class="container carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
 
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <a href="" target="_blank">
+                    <img src="images/stock/silla1.svg" class="d-block w-100" alt="...">
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="" target="_blank">
+                    <img src="images/stock/congeladora1.svg" class="d-block w-100" alt="...">
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="" target="_blank">
+                    <img src="images/stock/sombrilla1.svg" class="d-block w-100" alt="...">
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="" target="_blank">
+                    <img src="images/stock/silla2.svg" class="d-block w-100" alt="...">
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="" target="_blank">
+                    <img src="images/stock/bolsa_de_dormir1.svg" class="d-block w-100" alt="...">
+                </a>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <div class="carousel-inner">
-        <!-- Asegúrate de que solo una imagen tenga la clase 'active' -->
-        <div class="carousel-item active">
-            <a href="" target="_blank">
-                <img src="images/stock/silla1.svg" class="d-block w-100" alt="...">
-            </a>
-        </div>
-        <div class="carousel-item">
-            <a href="" target="_blank">
-                <img src="images/stock/congeladora1.svg" class="d-block w-100" alt="...">
-            </a>
-        </div>
-        <div class="carousel-item">
-            <a href="" target="_blank">
-                <img src="images/stock/sombrilla1.svg" class="d-block w-100" alt="...">
-            </a>
-        </div>
-        <div class="carousel-item">
-            <a href="" target="_blank">
-                <img src="images/stock/silla2.svg" class="d-block w-100" alt="...">
-            </a>
-        </div>
-        <div class="carousel-item">
-            <a href="" target="_blank">
-                <img src="images/stock/bolsa_de_dormir1.svg" class="d-block w-100" alt="...">
-            </a>
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
 
     <!-- /////////////////////////////////////////
         //          BANNER LLAMATIVO           //
@@ -143,62 +142,12 @@
         //          DETALLES DEL ENVIO          //
         /////////////////////////////////////////-->
     <!-- DETALLES DE ENVIO -->
-<div class="container d-flex flex-column flex-md-row gap-3">
-    <!-- CARTA 1 -->
-    <div class="container carta d-flex flex-column align-items-center text-center">
-        <!-- IMAGEN -->
-        <div class="container imagen-carta">
-            <i class="fab fa-whatsapp fa-4x text-success"></i>
-        </div>
-        <!-- INFO -->
-        <div class="container">
-            <h5>Para consultas, cambios o devoluciones</h5>
-            <p>Escribinos en nuestro whatsapp</p>
-        </div>
-    </div>
-
-    <!-- CARTA 2 -->
-    <div class="container carta d-flex flex-column align-items-center text-center">
-        <!-- IMAGEN -->
-        <div class="container imagen-carta">
-            <i class="fas fa-credit-card fa-4x text-primary"></i>
-        </div>
-        <!-- INFO -->
-        <div class="container">
-            <h5>Paga con tarjeta y efectivo</h5>
-            <p>Oferta en 3, 6 y 12 cuotas</p>
-        </div>
-    </div>
-
-    <!-- CARTA 3 -->
-    <div class="container carta d-flex flex-column align-items-center text-center">
-        <!-- IMAGEN -->
-        <div class="container imagen-carta">
-            <i class="fas fa-truck fa-4x text-secondary"></i>
-        </div>
-        <!-- INFO -->
-        <div class="container">
-            <h5>Envios a todo el país</h5>
-            <p>Elegi la forma de entregar que quieras, ¡Y Listo!</p>
-        </div>
-    </div>
-</div>
-
-
-    <!-- /////////////////////////////////////////
-        //           MEDIOS DE PAGO            //
-        /////////////////////////////////////////-->
-<div class="container medios-de-pago d-flex flex-column justify-content-center align-items-center text-center d-none d-md-flex">
-    <h2>MEDIOS DE PAGO</h2>
-    <h3>Todas las facilidades de pago</h3>
-    <h4>Elegí la que más te convenga</h4>
-
-    <div class="container d-flex flex-column flex-md-row">
+    <div class="container d-flex flex-column flex-md-row gap-3">
         <!-- CARTA 1 -->
         <div class="container carta d-flex flex-column align-items-center text-center">
             <!-- IMAGEN -->
             <div class="container imagen-carta">
-                <img src="images/aguera.jpg" alt="">
+                <i class="fab fa-whatsapp fa-4x text-success"></i>
             </div>
             <!-- INFO -->
             <div class="container">
@@ -206,12 +155,12 @@
                 <p>Escribinos en nuestro whatsapp</p>
             </div>
         </div>
-    
+
         <!-- CARTA 2 -->
         <div class="container carta d-flex flex-column align-items-center text-center">
             <!-- IMAGEN -->
             <div class="container imagen-carta">
-                <img src="images/aguera.jpg" alt="">
+                <i class="fas fa-credit-card fa-4x text-primary"></i>
             </div>
             <!-- INFO -->
             <div class="container">
@@ -219,82 +168,132 @@
                 <p>Oferta en 3, 6 y 12 cuotas</p>
             </div>
         </div>
-    
+
         <!-- CARTA 3 -->
         <div class="container carta d-flex flex-column align-items-center text-center">
             <!-- IMAGEN -->
             <div class="container imagen-carta">
-                <img src="images/aguera.jpg" alt="">
+                <i class="fas fa-truck fa-4x text-secondary"></i>
             </div>
             <!-- INFO -->
             <div class="container">
-                <h5>Envios a todo el país, 30 días de prueba</h5>
+                <h5>Envios a todo el país</h5>
                 <p>Elegi la forma de entregar que quieras, ¡Y Listo!</p>
             </div>
         </div>
     </div>
-</div>
+
+
+    <!-- /////////////////////////////////////////
+        //           MEDIOS DE PAGO            //
+        /////////////////////////////////////////-->
+    <div class="container medios-de-pago d-flex flex-column justify-content-center align-items-center text-center d-none d-md-flex">
+        <h2>MEDIOS DE PAGO</h2>
+        <h3>Todas las facilidades de pago</h3>
+        <h4>Elegí la que más te convenga</h4>
+
+        <div class="container d-flex flex-column flex-md-row">
+            <!-- CARTA 1 -->
+            <div class="container carta d-flex flex-column align-items-center text-center">
+                <!-- IMAGEN -->
+                <div class="container imagen-carta">
+                    <img src="images/aguera.jpg" alt="">
+                </div>
+                <!-- INFO -->
+                <div class="container">
+                    <h5>Para consultas, cambios o devoluciones</h5>
+                    <p>Escribinos en nuestro whatsapp</p>
+                </div>
+            </div>
+        
+            <!-- CARTA 2 -->
+            <div class="container carta d-flex flex-column align-items-center text-center">
+                <!-- IMAGEN -->
+                <div class="container imagen-carta">
+                    <img src="images/aguera.jpg" alt="">
+                </div>
+                <!-- INFO -->
+                <div class="container">
+                    <h5>Paga con tarjeta y efectivo</h5>
+                    <p>Oferta en 3, 6 y 12 cuotas</p>
+                </div>
+            </div>
+        
+            <!-- CARTA 3 -->
+            <div class="container carta d-flex flex-column align-items-center text-center">
+                <!-- IMAGEN -->
+                <div class="container imagen-carta">
+                    <img src="images/aguera.jpg" alt="">
+                </div>
+                <!-- INFO -->
+                <div class="container">
+                    <h5>Envios a todo el país, 30 días de prueba</h5>
+                    <p>Elegi la forma de entregar que quieras, ¡Y Listo!</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
     <!-- /////////////////////////////////////////
         //           MEDIOS DE ENVIO           //
         /////////////////////////////////////////-->
-<div class="container medios-de-envio d-flex flex-column align-items-center text-center d-none d-md-flex">
-    <h2>MEDIOS DE ENVIO</h2>
-    <h3>Todas las facilidades de pago</h3>
-    <h4>Elegí la que más te convenga</h4>
+    <div class="container medios-de-envio d-flex flex-column align-items-center text-center d-none d-md-flex">
+        <h2>MEDIOS DE ENVIO</h2>
+        <h3>Todas las facilidades de pago</h3>
+        <h4>Elegí la que más te convenga</h4>
 
-    <div class="container d-flex flex-column flex-md-row">
-        <!-- CARTA 1 -->
-        <div class="container carta d-flex flex-column align-items-center text-center">
-            <!-- IMAGEN -->
-            <div class="container imagen-carta">
-                <img src="images/gtr.jpg" alt="">
+        <div class="container d-flex flex-column flex-md-row">
+            <!-- CARTA 1 -->
+            <div class="container carta d-flex flex-column align-items-center text-center">
+                <!-- IMAGEN -->
+                <div class="container imagen-carta">
+                    <img src="images/gtr.jpg" alt="">
+                </div>
+                <!-- INFO -->
+                <div class="container">
+                    <h5>Para consultas, cambios o devoluciones</h5>
+                    <p>Escribinos en nuestro whatsapp</p>
+                </div>
             </div>
-            <!-- INFO -->
-            <div class="container">
-                <h5>Para consultas, cambios o devoluciones</h5>
-                <p>Escribinos en nuestro whatsapp</p>
+        
+            <!-- CARTA 2 -->
+            <div class="container carta d-flex flex-column align-items-center text-center">
+                <!-- IMAGEN -->
+                <div class="container imagen-carta">
+                    <img src="images/gtr.jpg" alt="">
+                </div>
+                <!-- INFO -->
+                <div class="container">
+                    <h5>Paga con tarjeta y efectivo</h5>
+                    <p>Oferta en 3, 6 y 12 cuotas</p>
+                </div>
+            </div>
+        
+            <!-- CARTA 3 -->
+            <div class="container carta d-flex flex-column align-items-center text-center">
+                <!-- IMAGEN -->
+                <div class="container imagen-carta">
+                    <img src="images/gtr.jpg" alt="">
+                </div>
+                <!-- INFO -->
+                <div class="container">
+                    <h5>Envios a todo el país, 30 días de prueba</h5>
+                    <p>Elegi la forma de entregar que quieras, ¡Y Listo!</p>
+                </div>
             </div>
         </div>
-    
-        <!-- CARTA 2 -->
-        <div class="container carta d-flex flex-column align-items-center text-center">
-            <!-- IMAGEN -->
-            <div class="container imagen-carta">
-                <img src="images/gtr.jpg" alt="">
-            </div>
-            <!-- INFO -->
-            <div class="container">
-                <h5>Paga con tarjeta y efectivo</h5>
-                <p>Oferta en 3, 6 y 12 cuotas</p>
-            </div>
-        </div>
-    
-        <!-- CARTA 3 -->
-        <div class="container carta d-flex flex-column align-items-center text-center">
-            <!-- IMAGEN -->
-            <div class="container imagen-carta">
-                <img src="images/gtr.jpg" alt="">
-            </div>
-            <!-- INFO -->
-            <div class="container">
-                <h5>Envios a todo el país, 30 días de prueba</h5>
-                <p>Elegi la forma de entregar que quieras, ¡Y Listo!</p>
-            </div>
+
+        <div class="container d-flex flex-column align-items-center text-center d-none d-md-flex">
+            <h3>Tu compra está protegida</h3>
+            <h4>Nos hacemos cargo si tenés un problema</h4>
+
+            <p>Sitio Web seguro certificado por Mercado Pago</p>
+            <p>30 días de prueba. Si no es lo que esperabas, te devolvemos tu dinero</p>
+            <p>Garantía oficial de fábrica gestionada por nosotros</p>
         </div>
     </div>
-
-    <div class="container d-flex flex-column align-items-center text-center d-none d-md-flex">
-        <h3>Tu compra está protegida</h3>
-        <h4>Nos hacemos cargo si tenés un problema</h4>
-
-        <p>Sitio Web seguro certificado por Mercado Pago</p>
-        <p>30 días de prueba. Si no es lo que esperabas, te devolvemos tu dinero</p>
-        <p>Garantía oficial de fábrica gestionada por nosotros</p>
-    </div>
-</div>
 
 
 
