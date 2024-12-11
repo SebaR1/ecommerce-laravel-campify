83dcfc27cdf7a0da70d998461690cb6ca23a13f3
IamJuanpax <jubritos@frba.utn.edu.ar>
2024-12-11 16:40:28 -0300

    arreglos y modificaciones

--- resources/views/vistaProducto.blade.php
+++ resources/views/vistaProducto.blade.php
@@ -1,7 +1,7 @@
 @include('header')
 
 <!-- Contenedor general -->
-<div class="container d-flex justify-content-center aligns-items-center mt-3 mb-4 gap-3">
+<div class="container d-flex justify-content-center aligns-items-center margenes gap-3">
         <!-- Contenedor de images ilustrativas -->
         <div class="col-6 d-flex flex-column justify-content-center aligns-items-center gap-3">
             <!-- Imagen -->
@@ -26,9 +26,9 @@
             </div>
             
             <!-- Formas de pago -->
-            <div class="">
+            <div class="mb-4 mt-4">
                 <h3 class="fs-6">Nuestras promociones bancarias</h3>
-                <h4 class="fs-6">12 Cuotas sin interes de $...</h4>
+                <h4 class="fs-6 color-cuotas">12 Cuotas sin interes de $...</h4>
                 <!-- Tarjetas en miniatura -->
                 <div class="tarjetas-chicas d-flex justify-content-start aligns-items-center gap-1">
                     <img src="images/gtr.jpg" alt="" class="img-fluid">
@@ -39,8 +39,11 @@
                     <img src="images/gtr.jpg" alt="" class="img-fluid">
                 </div>
             </div>
+
+            <hr>
+
             <!-- Cosas de envio -->
-            <div class="envios">
+            <div class="envios mt-1 mb-3">
                 <div class="d-flex justify-content-start align-items-center gap-2">
                     <i class="fa-solid fa-truck fa-lg text-secondary"></i>
                     <p>Envio gratis a ...</p>
@@ -53,7 +56,7 @@
                     </div>
                 </div>
             </div>
-            <button class="btn mt-3">Comprar</button>
+            <button class="btn mt-2">Comprar</button>
         </div>
 </div>
 
