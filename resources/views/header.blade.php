71bb173b73a35b3ff469525130cc167c2c90f5f2
SebastianRomero <romerosebastian781@gmail.com>
2024-12-10 22:43:17 -0300

    agrego mas rutas

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -25,7 +25,7 @@
 
         <nav class="navbar navbar-expand-md bg-body-tertiary p-ajuste-navbar">
             <div class="container-fluid">
-                <a class="navbar-brand p-ajuste-logo" href="#">
+                <a class="navbar-brand p-ajuste-logo" href="{{route('/')}}">
                     <img src="images/logosinbg.png" alt="Logo" class="m-ajuste-logo logo">
                 </a>
                 <button class="navbar-toggler m-ajuste-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,7 +37,7 @@
                             <a class="nav-link" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item ajuste-item-nav-md">
-                            <a class="nav-link" href="#">Sobre Nosotros</a>
+                            <a class="nav-link" href="{{route('sobreNosotros')}}">Sobre Nosotros</a>
                         </li>
                         <li class="nav-item ajuste-item-nav-md">
                             <a class="nav-link" href="#">Trabaja con nosotros</a>
@@ -86,83 +86,6 @@
                 </div>
             </div>
         </nav>
-        
-
-        <!--
-
-        <nav class="navbar navbar-expand-md bg-body-tertiary">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">Navbar</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul id="linksAdicionales" class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Dropdown
-                        </a>
-                <ul class="dropdown-menu">
-                          <li><a class="dropdown-item" href="#">Action</a></li>
-                          <li><a class="dropdown-item" href="#">Another action</a></li>
-                          <li><hr class="dropdown-divider"></li>
-                          <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                      </li>
-                    
-                    </li>
-                </ul>
-                <form class="d-flex mx-auto order-2 order-md-1" role="search">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 order-1 order-md-2 d-flex">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="">Iniciar Sesion</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="">Crear Cuenta</a>
-                    </li>
-                </ul>
-                
-                
-              </div>
-            </div>
-          </nav>
-
-        -->
-
-        <!--
-
-        <nav class="navbar navbar-expand-lg navbar-custom">
-            <div class="container d-flex align-items-center justify-content-between p-0">
-                // Logo 
-                <a class="navbar-brand align-items-center justify-content-center d-flex" href="{{ route('/') }}">
-                    <img src="images/logosinbg.png" alt="Logo" class=" logo">
-                </a>
-                
-                // Contenido del navbar
-                <div class="d-flex align-items-center gap-3 flex-grow-1 justify-content-end">
-                    // Barra de búsqueda visible solo en pantallas grandes
-                    <div class="input-group d-none d-md-flex">
-                        <input type="text" class="form-control" placeholder="Buscar..." aria-label="Buscar">
-                        <button class="btn btn-primary" type="button">Buscar</button>
-                    </div>
-                    //Botón Iniciar Sesión
-                    <button class="btn btn-primary" style="white-space: nowrap;">Iniciar Sesión</button>
-                </div>
-            </div>
-
-            
-        </nav>
-        -->
-        
 
             <!-- /////////////////////////////////////////
         //    ARTUCLOS CON MENU DESPLEGABLE     //
