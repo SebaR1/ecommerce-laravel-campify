55a773709759f002806d4663a70199b801176d08
SebastianRomero <romerosebastian781@gmail.com>
2024-12-12 20:03:22 -0300

    loginController

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function registro(Request $request)
+    {
+        $data = $request->validate([
+                "username" => ['required', 'min:2', 'max:12', 'alpha_dash'],
+                "email" => ["required", "email"],
+                "password" => ['required', 'regex:/^(?=.*[A-Za-z])(?=.*\d).+$/', 'confirmed']
+            ],
+            [
+                "username.required" => "Este campo es obligatorio",
+                "username.min" => "Este campo necesita más de 2 caracteres",
+                "username.max" => "Este campo no puede tener màs de 12 caracteres",
+                "username.alpha_dash" => "Este campo solo soporte valores alfanumericos y guiones",
+                "email.required" => "Este campo es obligatorio",
+                "email.email" => "Este campo tiene que ser un email",
+                "password.required" => "Este campo es obligatorio",
+                "password.regex" => "La contraseña necesita minimo 1 letra y 1 numero!",
+                "password.confirmed" => "Las contraseñas no coinciden"
+            ]
+        );
+        
+        $data["password"] = bcrypt($data["password"]);
+
+        User::create($data);
+
+        return response()->redirectTo("inicioSesion")->with("success", "Se registro con exito!");
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            "loginUsername" => ['required'],
+            "loginPassword" => ['required']
+        ],
+        [
+            "username.required" => "Este campo es obligatorio",
+            "password.required" => "Este campo es obligatorio",
+        ]
+        );
+
+        if(Auth::attempt(["username" => $data["loginUsername"], "password" => $data["loginPassword"]]))
+        {
+            $request->session()->regenerate();
+            return response()->redirectTo("/")->with("success", "FUNCIONOOOOOOOO");
+        }
+        else{
+            //fallo!
+            return response()->redirectTo("/")->with("fail", "NO FUNCIONOOOOOOOO, OSEA SI PERO NO LO ENCONTRO!");
+        }
+    }
+}
