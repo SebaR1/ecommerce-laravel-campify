f72168864df35a9dafcb97257e79f16e24775724
IamJuanpax <jubritos@frba.utn.edu.ar>
2024-12-13 00:01:18 -0300

    Retoques para multiplataforma

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -8,67 +8,59 @@
 
 <!-- Vista de los productos -->
 <div class="container d-flex flex-column justify-content-center align-items-center gap-3 mt-3 mb-5">
-    <!-- fila de 4 items -->
-    <div class="d-flex justify-content-center align-items-center gap-3">
+    <!-- fila de tarjetas -->
+    <div class="row w-100 justify-content-center">
         <!-- Tarjeta -->
-        <a href="#" class="tarjeta-link">
-            <div class="container d-flex flex-column justify-content-center align-items-center gap-3 border tarjeta-producto">
-                <img src="images/gtr.jpg" alt="Imagen del producto" class="img-fluid">
-                <div class="d-flex flex-column justify-content-center align-items-center text-center">
-                    <h3>Nombre del producto</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus odit aliquid vero 
-                        tenetur ab natus repudiandae quod minus suscipit, ex reprehenderit, voluptas sit tempore. 
-                        Voluptas nihil eaque fuga vel excepturi!
-                    </p>
+        <div class="col-12 col-md-6 col-lg-3 mb-3">
+            <a href="#" class="tarjeta-link">
+                <div class="container d-flex flex-column justify-content-center align-items-center gap-3 border tarjeta-producto">
+                    <img src="images/gtr.jpg" alt="Imagen del producto" class="img-fluid">
+                    <div class="d-flex flex-column justify-content-center align-items-center text-center">
+                        <h3>Nombre del producto</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus odit aliquid...</p>
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
 
         <!-- Tarjeta -->
-        <a href="#" class="tarjeta-link">
-            <div class="container d-flex flex-column justify-content-center align-items-center gap-3 border tarjeta-producto">
-                <img src="images/gtr.jpg" alt="Imagen del producto" class="img-fluid">
-                <div class="d-flex flex-column justify-content-center align-items-center text-center">
-                    <h3>Nombre del producto</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus odit aliquid vero 
-                        tenetur ab natus repudiandae quod minus suscipit, ex reprehenderit, voluptas sit tempore. 
-                        Voluptas nihil eaque fuga vel excepturi!
-                    </p>
+        <div class="col-12 col-md-6 col-lg-3 mb-3">
+            <a href="#" class="tarjeta-link">
+                <div class="container d-flex flex-column justify-content-center align-items-center gap-3 border tarjeta-producto">
+                    <img src="images/gtr.jpg" alt="Imagen del producto" class="img-fluid">
+                    <div class="d-flex flex-column justify-content-center align-items-center text-center">
+                        <h3>Nombre del producto</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus odit aliquid...</p>
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
 
         <!-- Tarjeta -->
-        <a href="#" class="tarjeta-link">
-            <div class="container d-flex flex-column justify-content-center align-items-center gap-3 border tarjeta-producto">
-                <img src="images/gtr.jpg" alt="Imagen del producto" class="img-fluid">
-                <div class="d-flex flex-column justify-content-center align-items-center text-center">
-                    <h3>Nombre del producto</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus odit aliquid vero 
-                        tenetur ab natus repudiandae quod minus suscipit, ex reprehenderit, voluptas sit tempore. 
-                        Voluptas nihil eaque fuga vel excepturi!
-                    </p>
+        <div class="col-12 col-md-6 col-lg-3 mb-3">
+            <a href="#" class="tarjeta-link">
+                <div class="container d-flex flex-column justify-content-center align-items-center gap-3 border tarjeta-producto">
+                    <img src="images/gtr.jpg" alt="Imagen del producto" class="img-fluid">
+                    <div class="d-flex flex-column justify-content-center align-items-center text-center">
+                        <h3>Nombre del producto</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus odit aliquid...</p>
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
 
         <!-- Tarjeta -->
-        <a href="#" class="tarjeta-link">
-            <div class="container d-flex flex-column justify-content-center align-items-center gap-3 border tarjeta-producto">
-                <img src="images/gtr.jpg" alt="Imagen del producto" class="img-fluid">
-                <div class="d-flex flex-column justify-content-center align-items-center text-center">
-                    <h3>Nombre del producto</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus odit aliquid vero 
-                        tenetur ab natus repudiandae quod minus suscipit, ex reprehenderit, voluptas sit tempore. 
-                        Voluptas nihil eaque fuga vel excepturi!
-                    </p>
+        <div class="col-12 col-md-6 col-lg-3 mb-3">
+            <a href="#" class="tarjeta-link">
+                <div class="container d-flex flex-column justify-content-center align-items-center gap-3 border tarjeta-producto">
+                    <img src="images/gtr.jpg" alt="Imagen del producto" class="img-fluid">
+                    <div class="d-flex flex-column justify-content-center align-items-center text-center">
+                        <h3>Nombre del producto</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus odit aliquid...</p>
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
 
     </div>
 </div>
