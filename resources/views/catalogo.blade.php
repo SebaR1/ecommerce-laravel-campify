5777c5adb6f9687725070ad9751775bf1193fdcb
IamJuanpax <jubritos@frba.utn.edu.ar>
2024-12-22 21:51:18 -0300

    comiteo esto para que me deje cambiar de rama

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -5,7 +5,7 @@
         <!-- Contenedor de Filtros -->
         <div class="col-12 col-md-2 d-flex flex-column gap-3 filtro">
             <h2 class="fw-bold border-bottom">Filtrar por:</h2>
-            <form action="">
+            <form action="" method="">
                 <label class="form-check">
                     <input class="form-check-input" type="checkbox" value="regalos-empresariales">
                     <span class="form-check-label">REGALOS EMPRESARIALES</span>
