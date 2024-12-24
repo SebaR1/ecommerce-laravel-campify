3f4194d030c2b241a87c4365ec2ce7871bb5d94f
Elias Olivencia <elias.olivencia@gmail.com>
2024-12-23 23:38:18 -0300

    Comienzo Linkeo

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -54,35 +54,39 @@
             <div class="row container_tarjetas">
                 @foreach ($productos as $producto)
                 <div class="col-12 col-sm-6 col-lg-4 mb-4">
-                    <div class="tarjeta-producto">
-                        <img src="{{ asset('storage/' . $producto->imagen_producto) }}" alt="Imagen del producto">
-                        <div class="container_datos_tarjeta">
-                            <h3>{{$producto->nombre_producto}}</h3>
-                            <p>{{$producto->categoria->nombre_categoria}}</p>
-                            <p>{{$producto->descripcion_producto}}</p>
-                            <p>
-                                ${{$producto->precio_producto}}
-                            </p>
-                            <div class="d-flex justify-content-evenly align-items-center">
-                                <div class="botonMeGusta">
-                                    <form method="POST" action="{{ route('me-gusta.toggle', ['producto_id' => $producto->id_producto]) }}">
-                                        @csrf
-                                        <button type="submit" class="btn_meGusta d-flex justify-content-center align-items-center {{ $producto->meGustas->contains('usuario_id', auth()->id()) ? 'active' : '' }}">
-                                            <i class="fa-solid fa-heart "></i>
-                                        </button>
-                                    </form>
-                                </div>
-                                <div class="container_btn_tarjeta">
-                                    <a href="#" class="btn_comprar">Comprar</a>
-                                </div>
-                                <div class="botonMeGusta">
-                                        <button type="submit" class="btn_meGusta d-flex justify-content-center align-items-center">
-                                            <i class="fa-solid fa-cart-shopping"></i>
-                                        </button>
-                                </div>
-                            </div> 
+                    <a href="{{route('producto.vistaProducto',$producto->id_producto)}}">
+                        <div class="tarjeta-producto">
+                            <img src="{{ asset('storage/' . $producto->imagen_producto) }}" alt="Imagen del producto">
+                            <div class="container_datos_tarjeta">
+                                <h3>{{$producto->nombre_producto}}</h3>
+                                <p>{{$producto->categoria->nombre_categoria}}</p>
+                                <p>{{$producto->descripcion_producto}}</p>
+                                <p>
+                                    ${{$producto->precio_producto}}
+                                </p>
+                                <div class="d-flex justify-content-evenly align-items-center">
+                                    <div class="botonMeGusta">
+                                        <form method="POST" action="{{ route('me-gusta.toggle', ['producto_id' => $producto->id_producto]) }}">
+                                            @csrf
+                                            <button type="submit" class="btn_meGusta d-flex justify-content-center align-items-center {{ $producto->meGustas->contains('usuario_id', auth()->id()) ? 'active' : '' }}">
+                                                <i class="fa-solid fa-heart "></i>
+                                            </button>
+                                        </form>
+                                    </div>
+                                    <div class="container_btn_tarjeta">
+                                        <a href="#" class="btn_comprar">Comprar</a>
+                                    </div>
+                                    <div class="botonMeGusta">
+                                            <button type="submit" class="btn_meGusta d-flex justify-content-center align-items-center">
+                                                <i class="fa-solid fa-cart-shopping"></i>
+                                            </button>
+                                    </div>
+                                </div> 
+                            </div>
                         </div>
-                    </div>
+
+                    </a>
+                    
                 </div>
                 @endforeach
             </div>
