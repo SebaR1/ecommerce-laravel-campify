1b6375e1e78d6c1168a06476bbe266bb0fad4ad1
Elias Olivencia <elias.olivencia@gmail.com>
2024-12-29 17:10:10 -0300

    Inicio finalizacion de compra

---
+++ resources/views/producto/checkoutCompra.blade.php
@@ -0,0 +1,29 @@
+@include ('header');
+    <body>
+        <div class="container-form-datos-factura">
+            <form action="">
+
+
+
+
+            </form>
+
+
+        </div>
+
+        <div class="container-tabla-carrito">
+
+            <div class="tabla-carrito">
+
+            </div>
+
+            <button>Finalizar compra</button>
+
+        </div>
+
+
+
+    </body>
+
+
+@include('footer');
