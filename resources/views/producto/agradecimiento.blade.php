98e2ba6da2cdea033298462e64454c4013b016c9
Elias Olivencia <elias.olivencia@gmail.com>
2024-12-30 12:31:11 -0300

    Cambios

--- resources/views/producto/agradecimiento.blade.php
+++ resources/views/producto/agradecimiento.blade.php
@@ -1,35 +1,51 @@
 @include ('header');
-
-<div class="container-gral-resumen-compra">
-    <h1>Resumen de tu compra</h1>
-
-    <div class="container-datos-gral">
-        <div class="container-datos-cliente">
-            <h3>Datos de la compra:</h3>
-            <p><strong>Nombre:</strong> {{ $datosComprador['nombre_factura'] }}</p>
-            <p><strong>Apellido:</strong> {{ $datosComprador['apellido_factura'] }}</p>
-            <p><strong>Email:</strong> {{ $datosComprador['email_factura'] }}</p>
-            <p><strong>Teléfono:</strong> {{ $datosComprador['telefono_factura'] }}</p>
-            <p><strong>Dirección:</strong> {{ $datosComprador['direccion_factura'] }}</p>
-        </div>
-    
-        <div class="container-datos-carrito">
-            <h3>Productos en el carrito:</h3>
-            @foreach($productosEnCarrito as $item)
+<body>
+    <div class="container-gral-resumen-compra">
+        <h1>Resumen de tu compra</h1>
+       
+        <div class="container-datos-gral">
+            <div class="container-datos-factura">
+                <h3>Datos de la compra:</h3>
+                <p><strong>Nombre:</strong> {{ $datosComprador['nombre_factura'] }}</p>
+                <p><strong>Apellido:</strong> {{ $datosComprador['apellido_factura'] }}</p>
+                <p><strong>Email:</strong> {{ $datosComprador['email_factura'] }}</p>
+                <p><strong>Teléfono:</strong> {{ $datosComprador['telefono_factura'] }}</p>
+                <p><strong>Dirección:</strong> {{ $datosComprador['direccion_factura'] }}</p>
+            </div>
+        
+            <div class="container-datos-factura">
+                <h3>Productos en el carrito:</h3>
+                @php
+                    $totalPrecio = 0;
+                @endphp
+                @foreach($productosEnCarrito as $item)
+                    <div class="datos-carrito-factura">
+                        <p><strong>Producto:</strong> {{ $item['producto']->nombre_producto }}</p>
+                        <p><strong>Cantidad:</strong> {{ $item['cantidad'] }}</p>
+                        <p><strong>Precio:</strong> ${{ $item['producto']->precio_producto }}</p>
+                    </div>
+                    @php
+                        $totalPrecio += $item['producto']->precio_producto * $item['cantidad'];
+                    @endphp
+                @endforeach
                 <div>
-                    <p><strong>Producto:</strong> {{ $item['producto']->nombre_producto }}</p>
-                    <p><strong>Cantidad:</strong> {{ $item['cantidad'] }}</p>
-                    <p><strong>Precio:</strong> ${{ $item['producto']->precio_producto }}</p>
+                    <p><strong>Total:</strong> ${{ $totalPrecio }}</p>
                 </div>
-            @endforeach
+    
+                
+             
+            </div>
+            
+        </div>
+        <div class="container-btn-finalizar-compra">
+            <a href="" class="btn btn-primary btn_form boton-enviar">Finalizar compra</a>
         </div>
     </div>
     
-</div>
+</body>
+
+
     
-
-    
-
-
-
 @include('footer');
+
+
